2be84d657f5ef399e6b7ca2166fc0b5d97dce024
Emmanuel Bett <emmanuelkbett3@gmail.com>
2024-02-08 00:57:25 +0300

    creating details screen

--- lib/src/core/global.dart
+++ lib/src/core/global.dart
@@ -1,8 +1,8 @@
-import 'package:flutter_cryptoapp/src/pages/details/controller.dart';
-import 'package:flutter_cryptoapp/src/pages/home/controller.dart';
+import 'package:flutter_cryptoapp/src/pages/coin/coins/controller.dart';
+import 'package:flutter_cryptoapp/src/pages/coin/details/controller.dart';
 import 'package:get/get.dart';
 
 void init() {
-  Get.lazyPut(() => HomeController());
+  Get.lazyPut(() => CoinsController());
   Get.lazyPut(() => DetailsController());
 }
